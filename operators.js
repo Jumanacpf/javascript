dfea066dafa3077296eab581927a2c4ec8895efd
jumanacpf <jumanacpf@gmail.com>
2024-02-21 11:37:49 +0530

    operators created

---
+++ operators.js
@@ -0,0 +1,54 @@
+// let a = 10
+// let b = 20
+
+// //a =20 b = 10
+
+// let c=a
+// a=a+b
+// b=c
+// a=a-b
+// console.log("value of a is ",a);
+// console.log("value of b is ",b); 
+
+
+
+let a=20
+let b=10
+// let sum=a+b
+// console.log("The sum is ",sum);
+
+// let diff=b-a
+// console.log("The differece is ",diff);
+
+// let prod=b*a
+// console.log("The product is ",prod);
+
+// let div=b/a
+// console.log("The result is ",div);
+
+let mod=b%a
+console.log("The result is ",mod);
+
+// let q=5
+// let r=2
+// q=q**r
+// console.log("The square of q is ",q);
+
+
+
+
+
+// let r=4
+// let diameter=r+r
+// let cicumference=2*3.14*r
+// let area=3.14*r*r
+// console.log("Diameter of circle : ",diameter)
+// console.log("Circumference of circle : ",cicumference)
+// console.log("Area of circle : ",area)
+
+
+// let p=50
+// let t=40
+// let r=10
+// si=(p*t*r)/100
+// console.log("Simple Interest is : ",si)
