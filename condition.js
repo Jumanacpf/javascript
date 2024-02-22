f217321c38ac371662d66d3c1ac1c691796bc2c9
jumanacpf <jumanacpf@gmail.com>
2024-02-22 11:31:54 +0530

    conditionn created

---
+++ condition.js
@@ -0,0 +1,54 @@
+// if-else statement
+
+// let a=5
+// let c=10
+// if (a>c){
+//     console.log("a is large number")
+// }
+// else{
+//     console.log("c is large number")
+// }
+
+
+// let q=5
+// if (q%2==0)
+// {
+//     console.log("q is even number")
+// }
+// else{
+//     console.log("q is odd number")
+// }
+
+
+// switchcase
+
+// let q=10
+// let a=50
+
+// switch(true)
+// {
+//     case q>a:
+//         console.log("largest number is ",q);
+//         break;
+//     case a>q:
+//         console.log("largest number is ",a);
+//         break;
+//     default:
+//         console.log("no nuumber");
+// }
+
+
+let r=-5;
+switch(true)
+{
+    case r>0:
+        console.log(r,"is positive")
+        break;
+    case r<0:
+        console.log(r,"is negative")
+        break;
+    case r==0:
+        console.log("zero")
+}
+
+
