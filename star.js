d784d2620b62deedc23b9b414118a211c406f4f7
jumanacpf <jumanacpf@gmail.com>
2024-02-27 11:32:05 +0530

    star pattern

---
+++ star.js
@@ -0,0 +1,25 @@
+// starpattern
+
+let q=8;
+let i;
+for (i=0;i<=q;i++){
+  if(i%2!==0){
+    console.log(" *".repeat(i))
+  }
+}
+
+
+
+let n="";
+for(let i=4;i<5;i++){
+  for (j=0;j<5;j++){
+    n+="*"
+  }
+  console.log(n)
+}
+let m="";
+for(let i=0;i<3;i++){
+    m+="*"+"   "+"*"+"\n"
+  }
+  console.log(m)
+  console.log(n)
