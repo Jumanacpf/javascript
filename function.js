fa7aa3cee831d32582c306359a12bc5e86e9fe1b
jumanacpf <jumanacpf@gmail.com>
2024-02-28 21:00:08 +0530

    functions created

---
+++ function.js
@@ -0,0 +1,35 @@
+// functiondefinition
+
+// function add(a, b) {
+//     return a+b
+// }
+
+
+// functionexpression
+
+// let  hello = function(a,b){
+//     return a+b
+// }
+// console.log(hello);
+
+
+// multithreading(asychronous function)
+
+// console.log("a");
+// setTimeout(()=>{
+//     console.log("hello wrold");
+// },2000)
+// console.log("b");
+
+
+function operations(a,b){
+    let p=a+b
+    let q=a-b
+    let r=a*b
+    let s=a/b
+    console.log("sum is : "+p)
+    console.log("differece is : "+q)
+    console.log("product is : "+r)
+    console.log("quotient is : "+s)
+}
+operations(5,2)
